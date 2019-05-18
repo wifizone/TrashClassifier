2579bc2657a3a2c7c511c77e847853c1956e99d6
Anton Poluianov <poluyanov.a.an@sberbank.ru>
2019-05-18 13:18:05 +0300

    Initial Commit

---
+++ TrashClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrashClassifier
+//
+//  Created by Anton Poluianov on 18/05/2019.
+//  Copyright © 2019 Anton Poluianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
